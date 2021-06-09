c508f15f6029169789b1528eeb0e07af8858509f
Rafael Pelegrini <arrobarafael@hotmail.com>
2021-06-09 16:28:19 -0300

    Feito esqueleto dos icones de viagem

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,18 @@
-import { Flex, Image } from '@chakra-ui/react';
+import { Box, Flex, Image, Stack, Text, Divider } from '@chakra-ui/react';
 import { Header } from '../components/Header';
+import { Travel } from '../components/Travel';
+import { TravelItem } from '../components/Travel/TravelItem';
 
 export default function Home() {
   return (
-    <>
+    <Flex direction="column" w={1440} mx="auto">
       <Header />
-      <h1>home</h1>
-    </>
+
+      <Image src="/Banner.png" />
+
+      <Travel />
+
+      <Divider />
+    </Flex>
   );
 }
